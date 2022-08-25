84262c9e5ee7dd93e8d1a427c6639a51198d552d
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-25 15:17:16 +0300

    Connect with mongoose database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 app.listen(process.env.PORT || 8000, () => {
@@ -10,6 +11,28 @@ app.listen(process.env.PORT || 8000, () => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+let uriDB = process.env.DB_URI;
+
+mongoose.connect(uriDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+db.on('error', (err) => {
+  console.log('Error: ', err);
+});
+
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
