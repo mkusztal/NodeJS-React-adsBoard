72a54c57ce441f9253f81ebd4572dae39487df15
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-26 16:25:07 +0300

    Add login endpoint

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -43,4 +43,30 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    if (
+      login &&
+      typeof login === 'string' &&
+      password &&
+      typeof password === 'string'
+    ) {
+      const user = await User.findOne({ login });
+
+      if (!user) {
+        res.status(400).json({ message: 'Login or password are incorrect!' });
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: 'Login successful!' });
+        } else {
+          res.status(400).json({ message: 'Login or password are incorrect!' });
+        }
+      }
+    } else {
+      res.status(400).json({ message: 'Bad request' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
