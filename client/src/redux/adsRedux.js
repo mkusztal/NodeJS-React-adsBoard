3e35364c9c3130679e4e57aa3567a9f18d5cd400
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-29 20:39:55 +0300

    Wrong connection actions creators with endpoints

--- client/src/redux/adsRedux.js
+++ client/src/redux/adsRedux.js
@@ -1,24 +1,46 @@
-// import axios from 'axios';
-// import { API_URL } from '../config';
+import { API_URL } from '../config';
+import shortid from 'shortid';
 
-export const getAds = ({ ads }) => ads.data;
+// selectors
+export const getAds = ({ ads }) => ads;
+export const getAdById = ({ ads }, adId) => ads.find((ad) => ad._id === adId);
 
-const reducerName = 'ads';
-const createActionName = (name) => `app/${reducerName}/${name}`;
+// actions
+const createActionName = (actionName) => `app/ads/${actionName}`;
+const ADD_AD = createActionName('ADD_AD');
+const EDIT_AD = createActionName('EDIT_AD');
+const UPDATE_AD = createActionName('UPDATE_AD');
+const REMOVE_AD = createActionName('REMOVE_AD');
 
-const LOAD_ADS = createActionName('LOAD_ADS');
+// action creators
+const addAd = (payload) => ({ type: ADD_AD, payload });
+const edit_ad = (payload) => ({ type: EDIT_AD, payload });
+const updateAdById = (payload) => ({ type: UPDATE_AD, payload });
+const removeAdById = (payload) => ({ type: REMOVE_AD, payload });
 
-export const loadAds = (payload) => ({ payload, type: LOAD_ADS });
-
-const initialState = {
-  data: [],
+export const fetchData = () => {
+  return (dispatch) => {
+    fetch(API_URL + 'api/ads')
+      .then((res) => res.json())
+      .then((ad) => dispatch(updateAdById(ad)));
+  };
 };
 
-export default function reducer(statePart = initialState, action = {}) {
+const adsReducer = (statePart = [], action) => {
   switch (action.type) {
-    case LOAD_ADS:
-      return { ...statePart, data: [...action.payload] };
+    case ADD_AD:
+      return [...statePart, { ...action.payload, id: shortid() }];
+    case EDIT_AD:
+      return statePart.map((ad) =>
+        ad.id === action.payload.id ? { ...ad, ...action.payload } : ad
+      );
+    case UPDATE_AD:
+      return [...action.payload];
+    case REMOVE_AD:
+      return statePart.filter((ad) => ad.id !== action.payload);
     default:
       return statePart;
   }
-}
+};
+
+export default adsReducer;
