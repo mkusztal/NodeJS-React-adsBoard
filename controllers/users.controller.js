b12e696e926e4d1ab98940e6a975b78a089a5b86
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-29 20:40:49 +0300

    Set login session when user is logged

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -73,9 +73,9 @@ exports.login = async (req, res) => {
         res.status(400).json({ message: 'Login or password are incorrect!' });
       } else {
         if (bcrypt.compareSync(password, user.password)) {
+          req.session.login = user.login;
           res.status(200).json({ message: 'Login successful!' });
         } else {
-          req.session.login = user.login;
           res.status(400).json({ message: 'Login or password are incorrect!' });
         }
       }
