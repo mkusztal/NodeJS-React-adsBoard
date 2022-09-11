9af9f2a799667f9e4d7429cf53efcff0bdb0c324
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-09-11 10:06:16 +0300

    Change url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,3 @@
-export const API_URL =
-  process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8000/api';
+export const API_URL = process.env.NODE_ENV === 'http://localhost:8000/api';
 export const IMAGES_URL =
-  process.env.NODE_ENV === 'production'
-    ? '/uploads/'
-    : 'http://localhost:8000/uploads/';
+  process.env.NODE_ENV === 'http://localhost:8000/uploads/';
